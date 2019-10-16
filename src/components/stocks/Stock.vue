e37d7a7d52237114639a506fca993d7ca0913952
games <gustavo.ames87@gmail.com>
2019-10-15 21:28:03 -0300

    stock component

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,11 +1,50 @@
 <template>
-    <h1>The Stock - Stock Component</h1>
+    <v-card style="margin: 10px"
+            shaped
+            elevation="8"
+        >
+
+        <v-card-title>{{ stock.cod }}</v-card-title>
+        <v-card-subtitle style="margin-left: 20px">{{ stock.name }}</v-card-subtitle>
+
+        <v-card-actions>
+            <div>
+                <v-card-text class="text--primary">
+                    <div>Price:</div>
+                    <div>{{ stock.buyPrice }}</div>
+                    <v-btn
+                            color="green"
+                            text>Buy
+                    </v-btn>
+                </v-card-text>
+            </div>
+            <div>
+                <v-card-text class="text--primary">
+                    <div>Price:</div>
+                    <div>{{ stock.sellPrice }}</div>
+                    <v-btn color="red"
+                            text>
+                        Sell
+                    </v-btn>
+                </v-card-text>
+            </div>
+            <v-text-field
+                    class="mt-0 pt-0"
+                    hide-details
+                    single-line
+                    type="number"
+                    value="100"
+                    style="width: 60px"
+            ></v-text-field>
+        </v-card-actions>
+
+    </v-card>
 </template>
 
 <script>
-    export default {
-        name: "Stock"
-    }
+export default {
+    props: ['stock']
+}
 </script>
 
 <style scoped>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,10 +1,66 @@
 <template>
-    <h1>The Stocks Component</h1>
+    <div>
+        <label></label>
+        <v-text-field
+                label="Search Stock:"
+                placeholder="Cod"
+                filled
+                rounded
+                dense
+                :append-icon="'mdi-share-outline'"
+        ></v-text-field>
+        <v-container>
+            <v-row>
+                    <stock
+                            v-bind:key="stock.cod"
+                            v-for="stock in stocks"
+                            :stock="stock"></stock>
+            </v-row>
+        </v-container>
+    </div>
 </template>
 
 <script>
+    import Stock from "./Stock";
     export default {
-        name: "Stocks"
+        name: "Stocks",
+        components: {Stock},
+        data: () => {
+            return {
+                stocks: [
+                    {
+                        cod: "AAPL34",
+                        name: "Apple",
+                        buyPrice: "97.61",
+                        sellPrice: "97.60"
+                    },
+                    {
+                        cod: "GOAU4",
+                        name: "Metalúrgica Gerdau",
+                        buyPrice: "6.37",
+                        sellPrice: "6.36"
+                    },
+                    {
+                        cod: "USIM5",
+                        name: "Usiminas",
+                        buyPrice: "7.79",
+                        sellPrice: "7.78"
+                    },
+                    {
+                        cod: "ITSA4",
+                        name: "Itaúsa",
+                        buyPrice: "12.93",
+                        sellPrice: "12.92"
+                    },
+                    {
+                        cod: "PETR4",
+                        name: "Petrobras",
+                        buyPrice: "27.60",
+                        sellPrice: "27.59"
+                    }
+                ]
+            }
+        }
     }
 </script>
 
